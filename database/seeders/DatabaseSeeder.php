eeaaf364052a477fb73560f1156f8379137156b8
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-02-07 04:04:53 +0900

    finish

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->times(50)->state([
+            'created_at' => fn () => now()->subMinutes(rand(0, 59)),
+        ])->create();
     }
 }
